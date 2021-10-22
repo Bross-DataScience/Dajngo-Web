52adf96d1fe95260602dfef081b3328474bc2869
Luigi_Bross <cursosinformatica9102@gmail.com>
2021-10-22 11:43:12 -0300

    Update arquivos

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse('<h1>Hello word</h1>')
+    return HttpResponse('<h1>Hello Django</h1>')
